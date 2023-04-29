c573db8a085f7fede4c22eff8fc38d9129c33906
Amit Mahajan <amitmahajan@post.com>
2023-04-29 20:14:18 +0100

    added docker file

--- datasource/shipping-order-api.js
+++ datasource/shipping-order-api.js
@@ -1,14 +1,40 @@
 const { RESTDataSource } = require('@apollo/datasource-rest');
-
+var request = require('request');
+var accessToken;
 class ShippingOrderAPI extends RESTDataSource {
   constructor() {
     super();
-    this.baseURL = 'http://localhost:4010/api/v1/order-processing/';
+    this.baseURL = process.env.SHIPPING_ORDER_API_ENDPOINT;
   }
 
   willSendRequest(_path, request) {
-    request.headers['authorization'] =
-      'Bearer ' + process.env.SHIPPING_ORDER_API_TOKEN;
+    this.getAuth0Token();
+    request.headers['authorization'] = 'Bearer ' + accessToken;
+  }
+
+  getAuth0Token() {
+    var accessTokenBody;
+    const body = {
+      client_id: process.env.AUTH0_CLIENT_ID,
+      client_secret: process.env.AUTH0_CLIENT_SECRET,
+      audience: 'http://logistics/resource/app',
+      grant_type: 'client_credentials',
+    };
+    var options = {
+      method: 'POST',
+      url: 'https://thesaastech.eu.auth0.com/oauth/token',
+      headers: { 'content-type': 'application/json' },
+      body: JSON.stringify(body),
+    };
+
+    request(options, function (error, response, body) {
+      if (error) throw new Error(error);
+
+      accessTokenBody = JSON.parse(body);
+      accessToken = accessTokenBody.access_token;
+    });
+
+    return accessToken;
   }
 
   getShippingOrders(shippingOrderLookupReq) {
